e103e7dd7dbd372aaade471c9e5266ea51697c20
Cemal Can İŞÇİ <cc.isci@hotmail.com>
2020-07-20 12:47:38 +0300

    netlify

--- src/Pages/Detail.js
+++ src/Pages/Detail.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { getPokemon,catchPokemon,releasePokemon } from '../Redux/actions';
 import EmptyData from '../Components/EmptyData';
-
+import typeColors from '../Constant';
 class Detail extends Component {
   componentDidMount() {
     const { get, match } = this.props;
@@ -27,14 +27,12 @@ class Detail extends Component {
     }
     catchPokemon(newPokeObject);
   };
-
   release = (name)=>(e)=>{
     const {releasePokemon} = this.props;
     releasePokemon(name);
   }
 
   render() {
-    
     const { match ,error } = this.props;
     const { params } = match;
     const { pokeId } = params;
@@ -85,26 +83,7 @@ class Detail extends Component {
       });
     }
 
-    const typeColors = {
-      bug: 'B1C12E',
-      dark: '4F3A2D',
-      dragon: '755EDF',
-      electric: 'FCBC17',
-      fairy: 'F4B1F4',
-      fighting: '823551D',
-      fire: 'E73B0C',
-      flying: 'A3B3F7',
-      ghost: '6060B2',
-      grass: '74C236',
-      ground: 'D3B357',
-      ice: 'A3E7FD',
-      normal: 'C8C4BC',
-      poison: '834594',
-      psychic: 'ED4882',
-      rock: 'B9A156',
-      steel: 'B5B5C3',
-      wwater: '3295F6',
-    };
+
 
     const heightMeter = height / 10;
     const weightKg = weight / 10;
